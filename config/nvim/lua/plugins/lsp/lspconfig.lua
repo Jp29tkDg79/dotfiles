d1b7be4e0ca24d0b8ef6587720a5f85d163e06f6
Jp29tkDg79 <tst11dbg29food79@gmail.com>
2023-10-30 20:55:02 +0900

    Fixed editconfig and yamlls

--- config/nvim/lua/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -56,7 +56,31 @@ return {
                 ---------------------
                 -- YAML & k8s
                 ---------------------
-                yamlls = {},
+                yamlls = {
+                    flags = { debounce_test_changes = 150 },
+                    settings = {
+                        yaml = {
+                            hover = true,
+                            completion = true,
+                            validate = true,
+                            schemas = {
+                                kubernetes = "globPattern",
+                                ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+                                ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+                                ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+                                ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+                                ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+                                ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+                                ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+                                ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+                                ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+                                ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+                                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+                                ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+                            },
+                        },
+                    },
+                },
                 ---------------------
                 -- HTML
                 ---------------------
