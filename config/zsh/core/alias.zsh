9ad65875e2e136753a57c242dbd64f58c954d6e3
Jp29tkDg79 <tst11dbg29food79@gmail.com>
2024-07-29 19:26:21 +0900

    Changed from exa to eza

--- config/zsh/core/alias.zsh
+++ config/zsh/core/alias.zsh
@@ -62,18 +62,18 @@ if [[ $(command -v bat) ]]; then
   alias cat="bat"
 fi
 
-# exa setup -------------
-if [[ $(command -v exa) ]]; then
-  alias e='exa --icons --git'
+# eza setup -------------
+if [[ $(command -v eza) ]]; then
+  alias e='eza --icons --git'
   alias l=e
   alias ls=e
-  alias ea='exa -a --icons --git'
+  alias ea='eza -a --icons --git'
   alias la=ea
-  alias ee='exa -aahl --icons --git'
+  alias ee='eza -aahl --icons --git'
   alias ll=ee
-  alias et='exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
+  alias et='eza -T -L 3 -a -I "node_modules|.git|.cache" --icons'
   alias lt=et
-  alias eta='exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
+  alias eta='eza -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
   alias lta=eta
   alias l='clear && ls'
 fi
